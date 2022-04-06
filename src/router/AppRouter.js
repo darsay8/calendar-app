bbeb1af7987e49e02d544a373c1cacd24c728c20
darsay8 <darsay.one@gmail.com>
2022-04-05 22:08:05 -0400

    route protection

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,24 +1,45 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import LoginScreen from '../components/auth/LoginScreen';
 import CalendarScreen from '../components/calendar/CalendarScreen';
 import { startChecking } from '../redux/actions/auth';
 import { useEffect } from 'react';
 
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
+
 export const AppRouter = () => {
   const dispatch = useDispatch();
+  const { checking, uid } = useSelector(state => state.auth);
+
   useEffect(() => {
     dispatch(startChecking());
   }, [dispatch]);
+
+  if (checking) {
+    return <h5>Loading...</h5>;
+  }
 
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/login" element={<LoginScreen />} />
-
-          <Route path="/" element={<CalendarScreen />} />
-
+          <Route
+            path="/login"
+            element={
+              <PublicRoute isAuth={!!uid}>
+                <LoginScreen />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute isAuth={!!uid}>
+                <CalendarScreen />
+              </PrivateRoute>
+            }
+          />
           <Route path="*" element={<Navigate replace to="/" />} />
         </Routes>
       </div>
