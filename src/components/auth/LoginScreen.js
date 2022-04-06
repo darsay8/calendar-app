0915ec4734f86839a1ca490dc5d002668e1ef99f
darsay8 <darsay.one@gmail.com>
2022-04-05 20:16:45 -0400

    register action

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,76 +1,126 @@
 import { useDispatch } from 'react-redux';
+import Swal from 'sweetalert2';
 import { useForm } from '../../hooks/useForm';
-import { startLogin } from '../../redux/actions/auth';
-import './login.css';
+import { startLogin, startRegister } from '../../redux/actions/auth';
 
 const LoginScreen = () => {
   const dispatch = useDispatch();
 
-  const [formLoginValues, handleLoginInputChange, reset] = useForm({
+  const [formLoginValues, handleLoginInputChange] = useForm({
     loginEmail: 'user@mail.com',
     loginPassword: '123ABC',
   });
 
+  const [formRegisterValues, handleRegisterInputChange] = useForm({
+    registerName: 'user',
+    registerEmail: 'user@mail.com',
+    registerPassword: '123ABC',
+    registerConfirmPassword: '123ABC',
+  });
+
   const { loginEmail, loginPassword } = formLoginValues;
+
+  const { registerName, registerEmail, registerPassword, registerConfirmPassword } =
+    formRegisterValues;
 
   const handleLogin = e => {
     e.preventDefault();
     dispatch(startLogin(loginEmail, loginPassword));
   };
 
+  const handleRegister = e => {
+    e.preventDefault();
+    if (registerPassword !== registerConfirmPassword) {
+      return Swal.fire('Error', 'Passwords should match', 'error');
+    }
+    dispatch(startRegister(registerName, registerEmail, registerPassword));
+  };
+
   return (
-    <div className="container login-container">
-      <div className="row">
-        <div className="col-md-6 login-form-1">
-          <h3>Login</h3>
-          <form className="form-horizontal" onSubmit={handleLogin}>
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Email"
-                name="loginEmail"
-                value={loginEmail}
-                onChange={handleLoginInputChange}
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Password"
-                name="loginPassword"
-                value={loginPassword}
-                onChange={handleLoginInputChange}
-              />
-            </div>
-            <div className="form-group">
-              <input type="submit" className="btnSubmit" value="Login" />
-            </div>
-          </form>
-        </div>
-
-        <div className="col-md-6 login-form-2">
-          <h3>Register</h3>
-          <form className="form-horizontal">
-            <div className="form-group">
-              <input type="text" className="form-control form-control" placeholder="Name" />
-            </div>
-            <div className="form-group">
-              <input type="email" className="form-control" placeholder="Email" />
-            </div>
-            <div className="form-group">
-              <input type="password" className="form-control" placeholder="Password" />
+    <div className="login-screen">
+      <div className="container">
+        <div className="row">
+          <div className="forms-group">
+            <div className="col-md-6 login-form-1">
+              <h3>Login</h3>
+              <form className="form-horizontal" onSubmit={handleLogin}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Email"
+                    name="loginEmail"
+                    value={loginEmail}
+                    onChange={handleLoginInputChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Password"
+                    name="loginPassword"
+                    value={loginPassword}
+                    onChange={handleLoginInputChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input type="submit" className="btnSubmit" value="Login" />
+                </div>
+              </form>
             </div>
 
-            <div className="form-group">
-              <input type="password" className="form-control" placeholder="Confirm password" />
-            </div>
+            <div className="col-md-6 login-form-2">
+              <h3>Register</h3>
+              <form className="form-horizontal" onSubmit={handleRegister}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control"
+                    placeholder="Name"
+                    name="registerName"
+                    value={registerName}
+                    onChange={handleRegisterInputChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Email"
+                    name="registerEmail"
+                    value={registerEmail}
+                    onChange={handleRegisterInputChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Password"
+                    name="registerPassword"
+                    value={registerPassword}
+                    onChange={handleRegisterInputChange}
+                  />
+                </div>
 
-            <div className="form-group">
-              <input type="submit" className="btnSubmit" value="Register" />
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Confirm password"
+                    name="registerConfirmPassword"
+                    value={registerConfirmPassword}
+                    onChange={handleRegisterInputChange}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <input type="submit" className="btnSubmit" value="Register" />
+                </div>
+              </form>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     </div>
--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,4 +1,5 @@
-import { fetchWithoutToken } from '../../helpers/fetch';
+import Swal from 'sweetalert2';
+import { fetchWithoutToken, fetchWithToken } from '../../helpers/fetch';
 import { types } from '../types/types';
 
 export const startLogin = (email, password) => async dispatch => {
@@ -9,6 +10,25 @@ export const startLogin = (email, password) => async dispatch => {
     localStorage.setItem('token', body.token);
     localStorage.setItem('token-init-date', new Date().getTime());
     dispatch(login({ uid: body.uid, name: body.name }));
+  } else {
+    console.log(body.errors);
+    const error = body.errors ? Object.values(body.errors)[0].msg : body.msg;
+    Swal.fire('Error', error, 'error');
+  }
+};
+
+export const startRegister = (name, email, password) => async dispatch => {
+  const res = await fetchWithoutToken('auth/new', { email, password, name }, 'POST');
+  const body = await res.json();
+
+  if (body.ok) {
+    localStorage.setItem('token', body.token);
+    localStorage.setItem('token-init-date', new Date().getTime());
+    dispatch(login({ uid: body.uid, name: body.name }));
+  } else {
+    console.log(body.errors);
+    const error = body.errors ? Object.values(body.errors)[0].msg : body.msg;
+    Swal.fire('Error', error, 'error');
   }
 };
 
